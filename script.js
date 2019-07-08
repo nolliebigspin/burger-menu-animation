2f2093e6c0a34788f18fca38f59ff1c274d604d0
nolliebigspin <alec007@t-online.de>
2019-07-09 00:29:38 +0200

    mögliche abgabe

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var burgerButton = document.documentElement;
+function burgerAnimation(burger) {
+    burger.classList.toggle("fadeOut");
+}
+
+function openMenu(menu) {
+    if (menu.classList.contains("fadeOut")) {
+        document.getElementById("menu").style.width = "400px";
+    } else {
+        document.getElementById("menu").style.width = "0px";
+    }
+}
